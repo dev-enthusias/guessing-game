8eef5e985885b1c647e5fbb7d1f01bcb612da487
Odogwu <becomingadev@gmail.com>
2022-10-18 06:57:55 +0100

    guessing game

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const checkBtn = document.querySelector('.check');
+const message = document.querySelector('.message');
+const scoreLabel = document.querySelector('.score');
+const secretNumberBox = document.querySelector('.number');
+const resetBtn = document.querySelector('.again');
+const inputVal = document.querySelector('.guess');
+const highscoreLabel = document.querySelector('.highscore');
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+checkBtn.addEventListener('click', function () {
+  const guess = Number(inputVal.value);
+
+  if (!guess) {
+    message.textContent = 'No Guess';
+  } else if (guess === secretNumber) {
+    message.textContent = 'Correct Score';
+    secretNumberBox.textContent = secretNumber;
+    secretNumberBox.style.width = '30rem';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    if (score > highscore) {
+      highscore = score;
+      highscoreLabel.textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    message.textContent = guess > secretNumber ? 'Too High' : 'Too Low';
+    score--;
+    scoreLabel.textContent = score;
+  }
+});
+
+resetBtn.addEventListener('click', function () {
+  score = 20;
+  secretNumberBox.textContent = '?';
+  inputVal.value = '';
+  message.textContent = 'Start guessing...';
+  scoreLabel.textContent = score;
+});
